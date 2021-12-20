c400c2aab10fcd241669b48fd2f951113cba7a29
mjmorrison10 <86391005+mjmorrison10@users.noreply.github.com>
2021-12-20 14:35:11 -0800

    addEventListener on map
    Added event listener on the map, then unhide the form. And upon clicking the form, add a marker to the map.

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent
+
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (pos) {
@@ -21,20 +23,48 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+     map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      //   L.marker(coords)
+      //     .addTo(map)
+      //     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      //     .openPopup();
+
+    //   Handling clicks on map
+      map.on('click', function (mapE) {
+          mapEvent = mapE;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+      });
     },
     function () {
       alert('Could not get your location');
     }
   );
 }
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  console.log(lat, lng);
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
